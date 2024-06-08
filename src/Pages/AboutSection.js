32b754319ce9602e0a912cabfc578c3a9fe77a12
Nicolasjeremy <nicolasjeremy11@gmail.com>
2024-06-08 11:07:54 +0700

    test resp

--- src/Pages/AboutSection.js
+++ src/Pages/AboutSection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../App.css";
-import "../Css/About.css";
 
 const AboutSection = () => {
   return (
--- src/Pages/MainSection.js
+++ src/Pages/MainSection.js
@@ -6,13 +6,13 @@ const MainSection = () => {
     <section className="main-section" id="home">
       <div className="intro">
         <img
-          src="../../FotoMain.png"
+          src={`${process.env.PUBLIC_URL}/FotoMain.png`}
           alt="Nicolas Jeremy"
           className="profile-pic"
         />
         <div className="intro-text">
           <h1>
-            Hello, I'm <span className="highlight">Nicolas Jeremy</span>
+            Hello, I'm <br /><span className="highlight">Nicolas Jeremy</span>
           </h1>
           <p>I'm a Junior Developer and Project manager.</p>
         </div>
